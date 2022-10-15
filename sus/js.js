64d2ecf6b38c92fdc77614c20a0802d902405877
diaszrew <104727551+diaszrew@users.noreply.github.com>
2022-10-15 11:25:01 -0300

    Add files via upload

---
+++ sus/js.js
@@ -0,0 +1,11 @@
+let valores = [8,4,6,1,6,7,1]
+/*
+for(let pos=0; pos < valores.length; pos++){
+    console.log(`aa posicao ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+
+
+for(let pos in valores){
+    console.log(`aa posicao ${pos} tem o valor ${valores[pos]}`)
+}
